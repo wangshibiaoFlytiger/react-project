ce5ea1f1a719e83c036b3c32f28cbbf3244db6f6
wangshibiao <645102170@qq.com>
2019-11-15 15:35:48 +0800

    feat: 引入ui库ant-design(antd)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Col, Layout, Row} from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Row>
+              <Col span={12}>col-12</Col>
+              <Col span={12}>col-12</Col>
+          </Row>
+          <Row>
+              <Col span={8}>col-8</Col>
+              <Col span={8}>col-8</Col>
+              <Col span={8}>col-8</Col>
+          </Row>
+          <Row>
+              <Col span={6}>col-6</Col>
+              <Col span={6}>col-6</Col>
+              <Col span={6}>col-6</Col>
+              <Col span={6}>col-6</Col>
+          </Row>
+      </div>
   );
 }
 
